7c294efacef6691bdc8bc432b5389e3b98b2596c
Орлов Ярослав <y.orlov@fonmix.ru>
2020-04-29 22:19:32 +0300

    'master: commit from script Ср апр 29 22:19:32 MSK 2020'

--- perl/Mojo-DOM/vicfirth.com/script.pl
+++ perl/Mojo-DOM/vicfirth.com/script.pl
@@ -14,7 +14,7 @@ use LWP::Simple;
 my $url = $ARGV[0];
 say $url;
 
-my $dir = './result/';
+my $dir = './result';
 my $tt = '%s/%s-%s.%s';
 
 my ($data_name_pref) = ( split('\/', $url) )[-1];
@@ -34,19 +34,20 @@ foreach my $p_data ( $dom->find('script.cue-playlist-data')->each ) {
     my $hash = decode_json($p_data->all_text);
 
     foreach my $item ( @{ $hash->{tracks} || {} } ) {
-        if ( $item->{title} =~ m/Bronze/ig ){
-            my $mp3_path = sprintf($tt, $dir, $data_name_pref, 'bronze', 'mp3');
-            getstore($item->{mp3}, $mp3_path);
-        } elsif ( $item->{title} =~ m/Silver/ig ){
-            my $mp3_path = sprintf($tt, $dir, $data_name_pref, 'silver', 'mp3');
-            getstore($item->{mp3}, $mp3_path);
-        } elsif ( $item->{artist} =~ m/Bronze/ig ){
-            my $mp3_path = sprintf($tt, $dir, $data_name_pref, 'app-bronze', 'mp3');
-            getstore($item->{mp3}, $mp3_path);
-        } elsif ( $item->{artist} =~ m/Silver/ig ){
-            my $mp3_path = sprintf($tt, $dir, $data_name_pref, 'app-silver', 'mp3');
-            getstore($item->{mp3}, $mp3_path);
-        }
+        my ($mp3_name) = $item->{mp3} =~ m/(\w+)\.mp3$/;
+        
+        my $preffix;
+        if ( $mp3_name =~ /bronze/ ) {
+            $preffix = 'bronze';
+            $preffix = 'bronzeapp' if ( $mp3_name =~ /bronzeapp/ );
+        } elsif ( $mp3_name =~ /silver/ ) {
+            $preffix = 'silver';
+            $preffix = 'silverapp' if ( $mp3_name =~ /silverapp/ );
+        };
+        next unless $preffix;
+        my $mp3_path = sprintf($tt, $dir, $data_name_pref, $preffix, 'mp3');
+        warn sprintf('%s -- %s', $item->{title}, $mp3_path);
+        getstore($item->{mp3}, $mp3_path);
     };
 };
 
@@ -65,12 +66,5 @@ sub send_request {
     my $ua = LWP::UserAgent->new();
     my $res = $ua->get($url);
     my $str = clean_str($res->decoded_content);
-    return $str;
-}
-
-sub clean_str {
-    my $str = shift;
-    $str =~ s/[\t\n]//g;
-    $str =~ s/\\\"//g;
     return $str;
 }

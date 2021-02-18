1222a701fa8e380eaeebc37792b8f008be39b3a9
Орлов Ярослав <y.orlov@fonmix.ru>
2021-02-18 19:57:49 +0300

    'master: commit from script Чт фев 18 19:57:49 MSK 2021'

--- python/book-simple-python/test.py
+++ python/book-simple-python/test.py
@@ -1,17 +1,65 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/echo/<param1>/<param2>')
-def echo(param1, param2):
-    return render_template('flask2.html', param1=param1, param2=param2)
+@app.route('/')
+def home():
+    kwargs = {}
+    kwargs['thing'] = request.args.get('thing')
+    kwargs['height'] = request.args.get('height')
+    kwargs['color'] = request.args.get('color')
+    return render_template('home.html', **kwargs)
+
+
+app.run(debug=True)
+
+'''
+import sys
+import requests
+from bs4 import BeautifulSoup as soup
+
+
+def get_links(url):
+    result = requests.get(url)
+    html = result.text
+    doc = soup(html)
+    print(type(doc))
+    links = []
+    for el in doc.find_all('a'):
+        links.append(el.get('href'))
+    return links
 
 
 if __name__ == '__main__':
-    app.run(port=9999, debug=True)
+    for url in sys.argv[1:]:
+        print(url)
+        for num, link in enumerate(get_links(url)):
+            print(num, link)
+'''
+
+# import webbrowser
+
+# url = 'https://python.org'
+# webbrowser.open_new_tab(url)
+
+# from flask import Flask, request, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route('/echo/')
+# def echo():
+#     kwargs = {}
+#     kwargs['param1'] = request.args.get('param1')
+#     kwargs['param2'] = request.args.get('param2')
+#     return render_template('flask2.html', **kwargs)
+
+
+# if __name__ == '__main__':
+#     app.run(port=9999, debug=True)
 
 '''
 from flask import Flask, render_template

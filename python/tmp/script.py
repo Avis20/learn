d683cd3567db901d3e855d73e66f38fa3583c705
Орлов Ярослав <y.orlov@fonmix.ru>
2021-03-09 16:26:05 +0300

    'master: commit from script Вт мар 9 16:26:05 MSK 2021'

---
+++ python/tmp/script.py
@@ -0,0 +1,16 @@
+
+
+print('hello, world')
+
+'''
+just_list = ['test1', 'test2']
+
+file_print = open('print.txt', 'wt')
+print(*just_list, file=file_print)
+
+
+
+file_write = open('write.txt', 'wt')
+file_write.write(','.join(just_list))
+
+'''

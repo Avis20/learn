25e94276a07c248f3f9fb1d81b7df03a1a71fda6
Орлов Ярослав <y.orlov@fonmix.ru>
2021-04-06 20:28:21 +0300

    'master: commit from script Вт апр 6 20:28:21 MSK 2021'

--- python/HakerRank.py
+++ python/HakerRank.py
@@ -1,14 +1,21 @@
 
+
+"""
+
 '''
 https://www.hackerrank.com/challenges/designer-door-mat/problem
 '''
 
 if __name__ == '__main__':
-    n, m = 7, 21
-    print('WELCOME')
+    n, m = map(int, input().split())
+    for i in range(1, (n // 2) * 2, 2):
+        print(('.|.'*i).center(m, '-'))
 
+    print('WELCOME'.center(m, '-'))
+    for i in range((n // 2) * 2 - 1, 0, -2):
+        print(('.|.'*i).center(m, '-'))
 
-
+"""
 
 '''
 """

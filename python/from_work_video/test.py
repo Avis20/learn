9dd93cc280fe063456ab67681f4e8a456e1cb93b
Орлов Ярослав <y.orlov@fonmix.ru>
2021-04-14 19:51:44 +0300

    'master: commit from script Ср апр 14 19:51:44 MSK 2021'

--- python/from_work_video/test.py
+++ python/from_work_video/test.py
@@ -1,27 +1,6 @@
-
-class BaseClass:
-
-    def __new__(cls, *args, **kwargs):
-        print("__new__ called")
-        print("\t-->", cls)
-        print("\t-->", kwargs)
-        print("\t-->", args)
-        return super().__new__(cls)
-
-    def __init__(self, name):
-        print("__init__ called")
-        self.name = name
-
-
-class ChildClass(BaseClass):
-
-    @classmethod
-    def construct(cls, name):
-        print("Class =", cls, "; Name =", name)
-        return cls(name)
-
+from pathlib import Path
 
 if __name__ == '__main__':
-    object_1 = ChildClass('Hello')
-    print('type=[', type(object_1), ']; object=[', object_1, ']', sep='')
-
+    cwd = Path.cwd()
+    file_path = cwd / 'test.txt'
+    print(file_path.read_text())
--- python/tmp/script.py
+++ python/tmp/script.py
@@ -1,25 +1,5 @@
 
 
-if __name__ == '__main__':
-    N = int(input())
-    array = []
-    for i in range(N):
-        params = str(input()).split(' ')
-        command = params[0]
-        del params[0]
-        params = list(map(int, params))
 
-        if command == 'insert':
-            array.insert(int(params[0]), params[1])
-        elif 'print' == command:
-            print(array)
-        elif 'remove' == command:
-            array.remove(params[0])
-        elif command == 'append':
-            array.append(params[0])
-        elif command == 'sort':
-            array.sort()
-        elif command == 'pop':
-            array.pop()
-        elif command == 'reverse':
-            array.reverse()
+
+

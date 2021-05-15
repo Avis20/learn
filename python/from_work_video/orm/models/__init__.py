00fb75f3ef0043f8a840590fb7d03a379b0749ee
Орлов Ярослав <y.orlov@fonmix.ru>
2021-05-15 23:29:26 +0300

    'master: init commit Сб 15 мая 2021 23:29:26 MSK'

--- python/from_work_video/orm/models/__init__.py
+++ python/from_work_video/orm/models/__init__.py
@@ -1,7 +1,13 @@
-from .user import User
 from .base import Base
+from .user import User
+from .post import Post
+from .tag import Tag
+from .base import Session
 
-# __all__ = [
-#     "Base",
-#     "User"
-# ]
+__all__ = [
+    "Base",
+    "User",
+    "Post",
+    "Tag",
+    "Session",
+]
--- python/from_work_video/orm/models/user.py
+++ python/from_work_video/orm/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from sqlalchemy import (
     Column,
@@ -17,6 +18,8 @@ class User(Base):
     name = Column(String(32), nullable=False)
     is_staff = Column(Boolean, nullable=False, server_default="0")
     create_at = Column(DateTime, default=datetime.utcnow())
+
+    posts = relationship("Post", back_populates="user")
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, name={self.name}, is_staff={self.is_staff})"
